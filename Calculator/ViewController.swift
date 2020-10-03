67a978b45e7d4dbcc06d2f0597be71e85d89b699
Mahmoud Sherbeny <sherbeny2096@gmail.com>
2020-10-03 15:42:55 +0200

    Finish BackEnd

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var restLblView: UILabel!
     
+    //MARK: - Variables
+    var operation: Int!
+    var numberOne: Float?
     
     
     //MARK: - Application LifeCycle
@@ -36,10 +39,71 @@ class ViewController: UIViewController {
     //MARK: - IBAction Methods
     @IBAction func buttonsAction(_ sender: Any) {
         
+        let buttonNum = (sender as! UIButton).tag
         
+        switch buttonNum {
+        case 0...9:
+            removeZero()
+            restLblView.text = restLblView.text! + "\(buttonNum)"
+            
+        case 10:
+            if !restLblView.text!.contains(".") {
+                restLblView.text = restLblView.text! + "."
+            }
+            
+        case 11:
+            guard let FN = numberOne else  {
+                return
+            }
+            restLblView.text = "\(getResult(FN: FN, SN: Float(restLblView.text!)!, operation: operation))"
+            
+        case 12...16:
+            numberOne = Float( restLblView.text! )
+            operation = buttonNum
+            clearResult()
+            
+        case 17:
+            if restLblView.text!.contains("-") {
+                restLblView.text!.removeFirst()
+            } else {
+                if restLblView.text! != "0" {
+                    restLblView.text! = "-" + restLblView.text!
+                }
+            }
+            
+        case 18:
+            clearResult()
+        default: break
+        }
         
     }
     
-   
+    //MARK: - Helper Methods
+    func clearResult() {
+        restLblView.text = "0"
+    }
+    
+    func removeZero() {
+        if restLblView.text == "0" {
+            restLblView.text = ""
+        }
+    }
+    
+    func getResult(FN: Float, SN: Float, operation: Int) -> Float {
+        switch operation {
+        case 12:
+            return FN + SN
+        case 13:
+            return FN - SN
+        case 14:
+            return FN * SN
+        case 15:
+            return FN / SN
+        case 16:
+            return Float(Int(FN) % Int(SN))
+        default:
+            return 0
+        }
+    }
 }
 
